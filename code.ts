f54db696e84a4c0dd2140cdd262c3d460241dff1
Tufan Calisir <tufan.calisir@project-joker.com>
2025-12-09 09:03:10 +0100

    feat: font weights

--- code.ts
+++ code.ts
@@ -1,5 +1,6 @@
 import { createRealColorsCssVariables } from './helpers/getColors';
 import { getTShirtVariables } from './helpers/getTShirtVariables';
+import { getFontWeights } from './helpers/getFontWeights';
 
 figma.showUI(__html__);
 
@@ -7,10 +8,11 @@ figma.ui.onmessage = async (msg: { type: string }) => {
 	if (msg.type === 'create-css-variables') {
 		const cssColorVariables = await createRealColorsCssVariables();
 		const cssDistanceVariables = getTShirtVariables();
+		const cssFontWeightVariables = getFontWeights();
 
 		figma.ui.postMessage({
 			type: 'css-generated',
-			css: [...cssColorVariables, ...cssDistanceVariables],
+			css: [...cssColorVariables, ...cssDistanceVariables, ...cssFontWeightVariables],
 		});
 	}
 
--- helpers/getFontWeights.ts
+++ helpers/getFontWeights.ts
@@ -1,22 +1,48 @@
 const getFontWeights = () => {
-	const FONT_WEIGHTS = ['100', '200', '300', '400', '500', '600', '700', '800', '900'];
+	const FONT_WEIGHTS = [
+		{ value: '100', name: 'thin' },
+		{ value: '200', name: 'extra-light' },
+		{ value: '300', name: 'light' },
+		{ value: '400', name: 'regular' },
+		{ value: '500', name: 'medium' },
+		{ value: '600', name: 'semi-bold' },
+		{ value: '700', name: 'bold' },
+		{ value: '800', name: 'extra-bold' },
+		{ value: '900', name: 'black' },
+	];
+	const variables: string[] = [];
 
-	const fontWeights: string[] = [];
+	const collectFontWeights = (): string[] => {
+		const fontWeights: string[] = [];
 
-	for (const node of figma.currentPage.findAll()) {
-		if (node.type === 'FRAME' && node.name.toLowerCase().includes('font weights')) {
-			if (node.children && node.children.length > 0) {
-				const firstChild = node.children[0];
-				if ('children' in firstChild && firstChild.children) {
-					for (const child of firstChild.children) {
-						if (child.type === 'TEXT') {
-							fontWeights.push(child.characters);
+		for (const node of figma.currentPage.findAll()) {
+			if (node.type === 'FRAME' && node.name.toLowerCase().includes('font weights')) {
+				if (node.children && node.children.length > 0) {
+					const firstChild = node.children[0];
+					if ('children' in firstChild && firstChild.children) {
+						for (const child of firstChild.children) {
+							if (child.type === 'TEXT') {
+								fontWeights.push(child.characters);
+							}
 						}
 					}
 				}
 			}
 		}
-	}
+
+		return fontWeights;
+	};
+
+	const fontWeights = collectFontWeights();
+
+	fontWeights.forEach((value) => {
+		const matchedWeight = FONT_WEIGHTS.find((weight) => weight.value === value);
+		if (matchedWeight) {
+			variables.push(`--font-weight-${matchedWeight.name}: ${value};`);
+		}
+	});
+
+	return variables;
 };
 
 export { getFontWeights };
